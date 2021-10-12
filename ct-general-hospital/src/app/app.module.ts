1f890448ac6b7cc0bea17159de39356887a538ec
Bhavya Gupta <BhavyaG2@citiustech.com>
2021-10-13 02:30:07 +0530

    admin changes-01

--- ct-general-hospital/src/app/app.module.ts
+++ ct-general-hospital/src/app/app.module.ts
@@ -1,6 +1,12 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatSelectModule} from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
 
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,8 +26,54 @@ import { AuthDirective } from './shared/directives/auth.directive';
 import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
 import { MedicalInformationComponent } from './components/medical-information/medical-information.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { EmployeeCardComponent } from './components/admin/employee-card/employee-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { PatientCardComponent } from './components/admin/patient-card/patient-card.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { FAQsComponent } from './shared/components/faqs/faqs.component';
+import { AddEmployeeComponent } from './components/admin/add-employee/add-employee.component';
+import { ChangePasswordComponent } from './shared/components/change-password/change-password.component';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { MyAccountComponent } from './shared/components/my-account/my-account.component';
+import { TermsConditionsComponent } from './shared/components/terms-conditions/terms-conditions.component';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const routes: Routes = [
+  {
+    path:'employee-card',
+    component:EmployeeCardComponent
+  },
+  {
+    path:'patient-card',
+    component:PatientCardComponent
+  },
+  {
+    path:'',
+    component:DashboardComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent
+  },
+{
+  path:'my-account',
+  component:MyAccountComponent
+},
+{
+  path:'change-password',
+  component:ChangePasswordComponent
+},
+{
+  path:'faqs',
+  component:FAQsComponent
+},
+{
+  path:'terms-conditions',
+  component:TermsConditionsComponent
+}
+
   // {
   //   path: '',
   //   redirectTo: '/home',
@@ -48,6 +100,15 @@ const routes: Routes = [
     CapitalizePipe,
     MedicalInformationComponent,
     AdminComponent,
+    EmployeeCardComponent,
+    PatientCardComponent,
+    DashboardComponent,
+    FAQsComponent,
+    AddEmployeeComponent,
+    ChangePasswordComponent,
+    MyAccountComponent,
+    TermsConditionsComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -56,8 +117,16 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule,
+    MatTabsModule,
     AngularMaterialModule,
     HttpClientModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatCardModule,
+    MatRadioModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ct-general-hospital/src/app/core/models/patient.model.ts
@@ -0,0 +1,11 @@
+export class Patient{
+  constructor(public id:number ,
+    public title: string,
+    public name:string,
+    public dob:Date,
+    public emailid:string,
+    public registeration:Date,
+    public status:string){
+  }
+  }
+  
