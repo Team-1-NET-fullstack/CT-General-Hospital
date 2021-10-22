0d08286534da8691ac1e1e9f5cfeb47ad6e0730a
Surabathuni Praveen kumar <SurabathuniP@citiustech.com>
2021-10-22 19:40:25 +0530

    feature commits for patiet visit

--- ct-general-hospital/src/app/components/patient/diagnosis-details/diagnosis-details.component.ts
+++ ct-general-hospital/src/app/components/patient/diagnosis-details/diagnosis-details.component.ts
@@ -11,41 +11,38 @@ import { DiagnosisDetails } from 'src/app/shared/models/diagnosis-details.model'
   styleUrls: ['./diagnosis-details.component.css'],
 })
 export class DiagnosisDetailsComponent implements OnInit {
+  diagnosisCode = new FormControl();
+  diagnosisCodeList: string[] = ['A00 - Cholera', 'A00.1 - Cholera due to Vibrio cholerae 01', 'B00 - biovar cholerae', 'T00 - Typhoid'];
   form: FormGroup = new FormGroup({});
-
+  listOfObjects: Object[] = [];
+  
   constructor(private medicalInformationService: MedicalInformationService) {}
 
   ngOnInit(): void {
-    let diagnosisCode= null;
-    let diagnosisDescription= null;
-    let isDepricated= false;
-    
+    let diagnosisCode = null;
+    let diagnosisDescription = null;
+    let isDepricated = false;
 
     this.form = new FormGroup({
-      diagnosisCode: new FormControl(diagnosisCode, [
-        Validators.required,
-      ]),
+      diagnosisCode: new FormControl(diagnosisCode, [Validators.required]),
       diagnosisDescription: new FormControl(diagnosisDescription, [
         Validators.required,
       ]),
-      isDepricated: new FormControl(isDepricated, [
-        Validators.required,
-      ]),
-     
+      isDepricated: new FormControl(isDepricated, [Validators.required]),
     });
   }
-  public hasError = (controlName: string, errorName: string) =>{
+  public hasError = (controlName: string, errorName: string) => {
     return this.form.controls[controlName].hasError(errorName);
-  }
-  saveDiagnosisDetails() {
+  };
+
+  add() {
     // Gathering data
     const patientVisitId = 1234;
     const diagnosisCode = this.form.value.diagnosisCode;
     const diagnosisDescription = this.form.value.diagnosisDescription;
     const isDepricated = this.form.value.isDepricated;
-    const updatedBy='robert@ctgeneral.com';
-    const insertedDate=new Date();
-    const id = this.form.value.id;
+    const updatedBy = 'robert@ctgeneral.com';
+    const insertedDate = new Date();
 
     // Create Ob
     const ob = new DiagnosisDetails(
@@ -54,12 +51,16 @@ export class DiagnosisDetailsComponent implements OnInit {
       diagnosisDescription,
       isDepricated,
       insertedDate,
-      updatedBy,
-       id,
+      updatedBy
     );
 
+    this.listOfObjects.push(ob);
+    console.log('Added to list');
     // Send to service
-   this.medicalInformationService.addDiagnosisDetails(ob);
-   alert("Record Added");
+  }
+
+  saveDiagnosisDetails() {
+    this.medicalInformationService.addDiagnosisDetails(this.listOfObjects);
+    alert('Record Added');
   }
 }
--- ct-general-hospital/src/app/shared/models/diagnosis-details.model.ts
+++ ct-general-hospital/src/app/shared/models/diagnosis-details.model.ts
@@ -1,11 +1,10 @@
 export class DiagnosisDetails {
-    constructor(
-        public patientVisitId:number,
-        public diagnosisCode: string,
-        public diagnosisDescription: string,
-        public isDepricated: boolean,
-        public insertedDate: Date,
-        public updatedBy:string,
-        public id:number,
-      ) {}
+  constructor(
+    public patientVisitId: number,
+    public diagnosisCode: string,
+    public diagnosisDescription: string,
+    public isDepricated: boolean,
+    public insertedDate: Date,
+    public updatedBy: string
+  ) {}
 }
